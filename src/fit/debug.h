f0dc4e2bafb9fdcceb733dc17f48c03246214cff
Spencer E. Olson <olsonse@umich.edu>
2005-01-14 14:10:15 +0000

    Documentation fixes.

--- src/fit/debug.h
+++ src/fit/debug.h
@@ -2,30 +2,32 @@
 
 #ifndef GA_DEBUG_H
 #define GA_DEBUG_H
-/**@name Debuging Tools
-*/
-//@{
+/**\file
+ * name Debuging Tools
+ */
 
-/// This function creates a pause in the program which requires user intervention.
+/** This function creates a pause in the program which requires user intervention. */
 int pausel();
 
-/// Force an exit with the exit status as 'status'.  Asks user to press enter.
+/** Force an exit with the exit status as 'status'.  Asks user to press enter. */
 void doexit(int status);
 
-/// An global variable to store the current debug level in.
+/** An global variable to store the current debug level in. */
 extern int debug_level;
 
-/// An global variable to store the default debug level in.
+/** An global variable to store the default debug level in. */
 extern int default_debug_level;
 
 /**@name Debug levels for fitting library:
 */
 //@{
-/// Absolutely no debug info.
+
+/** Absolutely no debug info. */
 const int GAR_NICHTS = 0;
-/// Debug the stinking Genetic Algorithm.
+
+/** Debug the stinking Genetic Algorithm. */
 const int GA = 2;
-//@}
 
 //@}
+
 #endif // GA_DEBUG_H
