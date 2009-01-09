6cca7ff825f531a5dc9367df2e3b10abc0763187
Spencer E. Olson <olsonse@umich.edu>
2009-01-09 00:26:54 +0000

    configuring boost.build for olson-tools (almost finished)

---
+++ src/olson-tools/logger.c
@@ -0,0 +1,499 @@
+// -*- c -*-
+// $Id: logger.c,v 1.1.1.1 2005/01/08 04:27:24 olsonse Exp $
+/*
+ * Partial Copyright 2000-2004 Spencer Eugene Olson --- All Rights Reserved
+ * Partial Copyright Neil Winton <neil@winton.org.uk>
+ *   Some of these logging routines are modified code from the Zebedee package
+ *   from Niel Winton.  These are thus release under GPL.
+ *
+ * $Log: logger.c,v $
+ * Revision 1.1.1.1  2005/01/08 04:27:24  olsonse
+ * Initial import
+ *
+ */
+
+/*********************\
+**		     **
+**  Message Logging  **
+**		     **
+\*********************/
+
+#include "logger.h"
+#include "fmacros.h"
+#define logger_c_rcsid "$Id: logger.c,v 1.1.1.1 2005/01/08 04:27:24 olsonse Exp $"
+
+#include <stdarg.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#ifdef __CYGWIN__
+#  undef WIN32
+#endif
+
+#ifdef WIN32
+/* I probably don't need all of these includes, but ... */
+#  include <windows.h>
+#  include <io.h>
+#  include <process.h>
+#  include <mmsystem.h>
+
+#elif defined(N_PLAT_NLM)
+#  error some more work needs to be done before this will compile for NetWare
+#else
+#  include <syslog.h>
+
+#  ifdef USE_PTHREAD
+#    include <pthread.h>
+#  endif
+
+#endif
+
+
+#ifdef LOG_MUTEX_TYPE
+LOG_MUTEX_TYPE * LogFileMutex = NULL;
+
+/* Make the logger routines fully re-entrant.
+ * Set the mutex to be used for the logger.
+ * This mutex must protect stdio
+ * calls as well as time.h calls (like strftime).
+ *
+ * By setting this to NULL, no mutexes will be used.
+ */
+void setLogFileMutex( LOG_MUTEX_TYPE * mutex)
+{
+    LogFileMutex = mutex;
+}
+
+#  ifdef WIN32
+#    define mutexLock(a)	EnterCriticalSection((a))
+#    define mutexUnlock(a)	LeaveCriticalSection((a))
+#  elif defined(N_PLAT_NLM)
+#  elif defined(USE_PTHREAD)
+#    define mutexLock(a)	pthread_mutex_lock((a))
+#    define mutexUnlock(a)	pthread_mutex_unlock((a))
+#  endif
+
+#  define MUTEX_IO LogFileMutex
+
+#else
+#  define mutexLock(a)
+#  define mutexUnlock(a)
+#endif
+
+FILE *LogFileP = NULL;		/* File handle for log file (NULL => stderr) */
+LogType_t LogFileType = LOGFILE_LOCAL;	/* Type of log file */
+LogLevel_t LogLevel = LLOG_INFO;	/* Message verbosity level */
+int TimestampLog = 0;		/* Should messages have timestamps? */
+char * ProgramName = NULL;
+
+
+void setLogProgramName(const char * prog)
+{
+    if (!prog) {
+	free(ProgramName);
+	ProgramName = NULL;
+	return;
+    } // else
+
+    ProgramName = realloc(ProgramName, strlen(prog) + 1);
+    memcpy(ProgramName, prog, strlen(prog) + 1);
+}
+
+
+/* create a function that is usable from fortran code. */
+void setLogProgramName_(const char * prog) {
+    setLogProgramName(prog);
+}
+
+#define Program (ProgramName != NULL ? ProgramName : "some cool program")
+
+/*
+** logThreadPid
+**
+** Return the current process ID
+*/
+
+unsigned long
+logThreadPid(void)
+{
+#ifdef WIN32
+    return (unsigned long)GetCurrentProcessId();
+#elif defined(N_PLAT_NLM)
+    return (unsigned long)GetNLMID();
+#else
+    return (unsigned long)getpid();
+#endif
+}
+
+/*
+** logThreadTid
+**
+** Return the current thread ID
+*/
+
+unsigned long
+LogThreadTid(void)
+{
+#ifdef WIN32
+    return (unsigned long)GetCurrentThreadId();
+#elif defined(N_PLAT_NLM)
+    return (unsigned long)GetThreadID();
+#elif defined(USE_PTHREAD)
+    return (unsigned long)pthread_self();
+#else
+    return 0;
+#endif
+}
+
+/*
+** timestamp
+**
+** Generate a time-stamp string
+*/
+
+void
+timestamp(char *timeBuf, int local)
+{
+    time_t now;
+    struct tm *tmPtr;
+
+    /* localtime()/gmtime are not thread-safe */
+
+    mutexLock(MUTEX_IO);
+    time(&now);
+    if (local)
+    {
+	tmPtr = localtime(&now);
+    }
+    else
+    {
+	tmPtr = gmtime(&now);
+    }
+    strftime(timeBuf, TIMESTAMP_SIZE, "%Y-%m-%d-%H:%M:%S", tmPtr);
+    mutexUnlock(MUTEX_IO);
+}
+
+#if !defined(N_PLAT_NLM)
+/*
+** logToSystemLog
+**
+** Write a message to the system logging facility. On Windows it goes to
+** the system application event log. Elsewhere is uses syslog().
+*/
+
+void
+logToSystemLog(LogLevel_t level, char *msg)
+{
+#ifdef WIN32
+    HANDLE  eventHandle;
+    char    *strings[2];
+
+
+    eventHandle = RegisterEventSource(NULL, Program);
+
+    strings[0] = msg;
+    strings[1] = NULL;
+
+    if (eventHandle != NULL)
+    {
+	ReportEvent(eventHandle,	    /* Handle of event source */
+		    (level == LLOG_SEVERE ? EVENTLOG_ERROR_TYPE :
+		     EVENTLOG_INFORMATION_TYPE),  /* Event type */
+		    (WORD)level,	    /* Event category */
+		    0,			    /* Event ID */
+		    NULL,		    /* User SID */
+		    1,			    /* Number of message strings */
+		    0,			    /* Bytes of binary data */
+		    (const char **)strings, /* Array of message strings */
+		    NULL);		    /* No binary data */
+	DeregisterEventSource(eventHandle);
+    }
+#else
+    int logLevel;
+
+    /*
+    ** Messages at level 0 are errors, 1 is notice, 2 informational
+    ** and everything else is classed as debug.
+    */
+
+    switch (level)
+    {
+    case LLOG_SEVERE:
+	logLevel = LOG_ERR;
+	break;
+
+    case LLOG_WARNING:
+	logLevel = LOG_NOTICE;
+	break;
+
+    case LLOG_INFO:
+	logLevel = LOG_INFO;
+	break;
+
+    default:
+	logLevel = LOG_DEBUG;
+	break;
+    }
+
+    syslog(logLevel, "%s", msg);
+#endif
+}
+#endif  // !N_PLAT_NLM
+
+#ifndef _predef_vlog
+void vlog(LogLevel_t level, int err, const char *fmt, va_list args);
+#  define _predef_vlog
+#endif
+
+/**
+ * log_severe
+ * 
+ * Log a message with the severe level.
+ */
+
+void
+log_severe(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vlog(LLOG_SEVERE, 0, fmt, args);
+    va_end(args);
+}
+
+#if OLSON_TOOLS_CAN_ALIAS
+    /* make available the weak symbols to allow fortran programs to use this.
+     * Unfortunately, not all binary formats allow for weak symbols.
+     * */
+void log_severe_ (const char *fmt, ...) __attribute__ (( alias ("log_severe") )) ;
+#else
+    /* take care of the lame binary formats. */
+void
+log_severe_(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vlog(LLOG_SEVERE, 0, fmt, args);
+    va_end(args);
+}
+#endif
+
+/**
+ * log_warning
+ * 
+ * Log a message with the warning level.
+ */
+
+void
+log_warning(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vlog(LLOG_WARNING, 0, fmt, args);
+    va_end(args);
+}
+
+/**
+ * log_info
+ * 
+ * Log a message with the info level.
+ */
+
+void
+log_info(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vlog(LLOG_INFO, 0, fmt, args);
+    va_end(args);
+}
+
+/**
+ * log_config
+ * 
+ * Log a message with the info level.
+ */
+
+void
+log_config(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vlog(LLOG_CONFIG, 0, fmt, args);
+    va_end(args);
+}
+
+/**
+ * log_fine
+ * 
+ * Log a message with the fine level.
+ */
+
+void
+log_fine(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vlog(LLOG_FINE, 0, fmt, args);
+    va_end(args);
+}
+
+/**
+ * log_finer
+ * 
+ * Log a message with the finer level.
+ */
+
+void
+log_finer(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vlog(LLOG_FINER, 0, fmt, args);
+    va_end(args);
+}
+
+/**
+ * log_finest
+ * 
+ * Log a message with the finest level.
+ */
+
+void
+log_finest(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vlog(LLOG_FINEST, 0, fmt, args);
+    va_end(args);
+}
+
+/**
+ * log
+ * 
+ * Log a message with the arbitrary level and error.
+ */
+
+void
+log_error(LogLevel_t level, int err, const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vlog(level, err, fmt, args);
+    va_end(args);
+}
+
+/*
+** vlog
+**
+** Output a message to the current log file if the message verbosity is
+** greater than or equal to the specified level. Messages at level 0
+** can not be suppressed (unless the log-file type is NULL) and are all
+** error messages.
+**
+** If errno is non-zero then append the matching error text.
+*/
+
+void
+vlog(LogLevel_t level, int err, const char *fmt, va_list args)
+{
+    char timeBuf[TIMESTAMP_SIZE];
+    char *timePtr = NULL;
+    /* hope that there are no longer messages */
+    static char msgBuf[2048];
+
+
+    if (level > LogLevel || LogFileType == LOGFILE_NULL) return;
+
+    if (TimestampLog)
+    {
+	timestamp(timeBuf, 1);
+	timePtr = timeBuf;
+    }
+
+    /* Ensure we don't get overlapping messages */
+    mutexLock(MUTEX_IO);
+
+    /*
+    ** The message format is the program name followed by the (low five
+    ** digits of) the PID and thread ID then an optional timestamp followed
+    ** by an amount of indentation determined by the level. This is
+    ** then followed by the supplied message text and arguments and
+    ** finally the error message text (if any) associated with the supplied
+    ** error number!
+    */
+
+    sprintf(msgBuf, "%s(%lu/%lu): %s%s%.*s%s",
+	    Program, (logThreadPid() % 100000), (LogThreadTid() % 100000),
+	    (timePtr ? timePtr : ""), (timePtr ? ": " : ""),
+	    (int)level, "          ",
+	    (level>LLOG_WARNING ? "" : ( level == LLOG_WARNING ? "Warning: ":"ERROR: "))
+	    );
+
+    vsprintf(msgBuf + strlen(msgBuf), fmt, args);
+
+    if (err)
+    {
+	sprintf(msgBuf + strlen(msgBuf), ": (%s)", strerror(err));
+    }
+
+    switch (LogFileType)
+    {
+    case LOGFILE_LOCAL:
+    {
+        FILE *fp = LogFileP != NULL ? LogFileP : stderr;
+	fprintf(fp, "%s\n", msgBuf);
+	fflush(fp);
+	break;
+    }
+
+#if !defined(N_PLAT_NLM)
+    case LOGFILE_SYSLOG:
+	logToSystemLog(level, msgBuf);
+	break;
+#endif
+
+    default:
+	break;
+    }
+
+    mutexUnlock(MUTEX_IO);
+
+}
+
+
+/** Set log file. */
+void setLogFile(const char * logfile) {
+    FILE * lfp = NULL;
+
+    if (logfile == NULL) {
+        if (LogFileP != NULL) fclose(LogFileP);
+        LogFileP = NULL;
+        LogFileType = LOGFILE_NULL;
+        return;
+    } else if (strncmp(logfile, ":stderr:", 8) == 0) {
+        if (LogFileP != NULL) fclose(LogFileP);
+        LogFileP = NULL;
+        LogFileType = LOGFILE_LOCAL;
+        return;
+    } else if (strncmp(logfile, ":syslog:", 8) == 0) {
+        if (LogFileP != NULL) fclose(LogFileP);
+        LogFileP = NULL;
+        LogFileType = LOGFILE_SYSLOG;
+        return;
+    }
+
+    lfp = fopen(logfile, "a");
+
+    if (!lfp) {
+        log_severe("cannot open log file '%s'", logfile);
+        return;
+    }
+
+    if (LogFileP != NULL) fclose(LogFileP);
+    LogFileP = lfp;
+    LogFileType = LOGFILE_LOCAL;
+}
+
---
+++ src/olson-tools/power.h
@@ -0,0 +1,145 @@
+#ifdef DUMMY_IFDEF_FOR_FORTRAN
+// -*- c -*-
+// $Id: power.h,v 1.2 2005/05/12 04:27:34 olsonse Exp $
+/*
+ * Copyright 2004 Spencer Eugene Olson --- All Rights Reserved
+ *
+ * $Log: power.h,v $
+ * Revision 1.2  2005/05/12 04:27:34  olsonse
+ * Fixed to for Intel 8.1 compilers.
+ * Found (using intel compiler) and fixed an array overflow in BField::potential.
+ * Didn't find it earlier because the array is on the stack for the function.
+ *
+ * Added fmacros.h file to simplify mixing fortran code with others.
+ * Added alias function names for Fortran interoperability.
+ *
+ * Revision 1.1.1.1  2005/01/08 04:27:24  olsonse
+ * Initial import
+ *
+ */
+#endif /* DUMMY_IFDEF_FOR_FORTRAN */
+
+/** \file
+ * pow(x,y) == \f$ x^{y} \f$.
+ * Copyright Spencer Olson 2004 -- All rights reserved.
+ *
+ * Insanity drove me to write my own pow function.
+ * This was actually my first assembly project.
+ *
+ * Note that this does not necessarily perform exactly according to ieee754 standards,
+ * but oh well. 
+ * 
+ * Timing: the examples compare the timing for my own pow() function with the
+ * pow() function that is built in with whatever compiler is used.  In my own
+ * experience, I haven't yet found an implementation that beats my with
+ * respect to execution time.  As an example, I typically observe that my
+ * routine is several times faster than the one provided by GCC. Although
+ * better, the Portland Group compiler does not close the gap much.  The
+ * closest comparison I've found is from Intel's compiler suite.  
+ * 
+ * Doing the following showed a total error of ~1.0e-12:
+ * \verbatim
+   x = 1.9
+   for y = -1000:.01:1000
+      tot_err += fabs( (pow(x,y) - fast_pow(x,y))/pow(x,y) )
+   end
+   \endverbatim
+ *
+ * With another test, I was able to get the total error up to 1.70605e-10 by
+ * \verbatim
+   for x = -100:0.1:110
+       for y = -150:0.01:150
+           tot_err += fabs( (pow(x,y) - fast_pow(x,y))/pow(x,y) )
+       end
+   end
+   \endverbatim
+ *
+ */
+
+/** \example fast_pow/c/testfast_pow.c
+ * fast_pow timing and tests in c. */
+
+/** \example fast_pow/fortran/testfast_pow.F
+ * fast_pow timing and tests in F77.
+ *
+ * \include fast_pow/fortran/stdpow.c
+ */
+
+#ifndef MY_OWN_POW
+#define MY_OWN_POW
+
+
+
+#ifdef __cplusplus
+namespace olson_tools {
+#endif 
+
+
+
+
+#ifdef SQR
+!  error  SQR already defined.  Better figure out what to do here now.
+#else
+   /** A faster way of computing \f$ x^{2} \f$. */
+#  ifdef __cplusplus
+    template <class T>
+    inline T SQR(const T & t) { return t * t; }
+#  else 
+#    define SQR(x)	((x)*(x))
+#  endif
+#endif
+
+#ifdef CUBE
+!  error  CUBE already defined.  Better figure out what to do here now.
+#else
+   /** A faster way of computing \f$ x^{3} \f$. */
+#  ifdef __cplusplus
+    template <class T>
+    inline T CUBE(const T & t) { return t * t * t; }
+#  else 
+#    define CUBE(x)	((x)*(x)*(x))
+#  endif
+#endif
+
+
+#  if !defined(LANGUAGE_FORTRAN__) || LANGUAGE_FORTRAN__ != 1
+
+#    ifdef __cplusplus
+extern "C" {
+#    endif 
+
+#if defined(USE_SPENCERS_FAST_POW) || defined(DOXYGEN_SKIP)
+
+/** My own pow function.
+ * Call this one for c/c++ code.  It will be ever so infinitesimally faster
+ * than fast_pow__ which is resolved to if "fast_pow" is used in fortran.
+ * @param x The base.
+ * @param y The exponent.
+ * @return \f$ x^{y} \f$
+ * */
+double fast_pow(double x,double y);
+
+/** This one is primarily for use from (g77) fortran code.
+ * It has only 2 additional mov instructions. */
+double fast_pow_(double*,double*);
+
+#else
+    /* the least we can do is provide an alias macro for the math lib pow
+     * function. */
+#  define fast_pow pow
+
+#endif /* USE_SPENCERS_FAST_POW */
+
+#  ifdef __cplusplus
+}
+#  endif 
+
+#endif /*  if LANGUAGE_FORTRAN__ != 1 */
+
+
+
+#ifdef __cplusplus
+}/* namespace olson_tools */
+#endif 
+
+#endif /* MY_OWN_POW */
