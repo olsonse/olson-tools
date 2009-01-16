e4f5c4e26d1b470f3dd7381f39c47b359c439e14
Spencer E. Olson <olsonse@umich.edu>
2009-01-16 21:55:45 +0000

    Many changes to get things to compile. Fixed most of Octree examples (except where a dependence on DSMC exists).
    Added octree::Walker::walk and octree::Walker::recursive_walk functions to
    aide in optimized octree walking.

--- src/olson-tools/random/MersenneTwister.h
+++ src/olson-tools/random/MersenneTwister.h
@@ -84,53 +84,53 @@ protected:
 
 //Methods
 public:
-	MTRand( const uint32& oneSeed );  // initialize with a simple uint32
-	MTRand( uint32 *const bigSeed, uint32 const seedLength = N );  // or an array
-	MTRand();  // auto-initialize with /dev/urandom or time() and clock()
+	inline MTRand( const uint32& oneSeed );  // initialize with a simple uint32
+	inline MTRand( uint32 *const bigSeed, uint32 const seedLength = N );  // or an array
+	inline MTRand();  // auto-initialize with /dev/urandom or time() and clock()
 	
 	// Do NOT use for CRYPTOGRAPHY without securely hashing several returned
 	// values together, otherwise the generator state can be learned after
 	// reading 624 consecutive values.
 	
 	// Access to 32-bit random numbers
-	double rand();                          // real number in [0,1]
-	double rand( const double& n );         // real number in [0,n]
-	double randExc();                       // real number in [0,1)
-	double randExc( const double& n );      // real number in [0,n)
-	double randDblExc();                    // real number in (0,1)
-	double randDblExc( const double& n );   // real number in (0,n)
-	uint32 randInt();                       // integer in [0,2^32-1]
-	uint32 randInt( const uint32& n );      // integer in [0,n] for n < 2^32
-	double operator()() { return rand(); }  // same as rand()
+	inline double rand();                          // real number in [0,1]
+	inline double rand( const double& n );         // real number in [0,n]
+	inline double randExc();                       // real number in [0,1)
+	inline double randExc( const double& n );      // real number in [0,n)
+	inline double randDblExc();                    // real number in (0,1)
+	inline double randDblExc( const double& n );   // real number in (0,n)
+	inline uint32 randInt();                       // integer in [0,2^32-1]
+	inline uint32 randInt( const uint32& n );      // integer in [0,n] for n < 2^32
+	inline double operator()() { return rand(); }  // same as rand()
 	
 	// Access to 53-bit random numbers (capacity of IEEE double precision)
-	double rand53();  // real number in [0,1)
+	inline double rand53();  // real number in [0,1)
 	
 	// Access to nonuniform random number distributions
-	double randNorm( const double& mean = 0.0, const double& variance = 0.0 );
+	inline double randNorm( const double& mean = 0.0, const double& variance = 0.0 );
 	
 	// Re-seeding functions with same behavior as initializers
-	void seed( const uint32 oneSeed );
-	void seed( uint32 *const bigSeed, const uint32 seedLength = N );
-	void seed();
+	inline void seed( const uint32 oneSeed );
+	inline void seed( uint32 *const bigSeed, const uint32 seedLength = N );
+	inline void seed();
 	
 	// Saving and loading generator state
-	void save( uint32* saveArray ) const;  // to array of size SAVE
-	void load( uint32 *const loadArray );  // from such array
-	friend std::ostream& operator<<( std::ostream& os, const MTRand& mtrand );
-	friend std::istream& operator>>( std::istream& is, MTRand& mtrand );
+	inline void save( uint32* saveArray ) const;  // to array of size SAVE
+	inline void load( uint32 *const loadArray );  // from such array
+	friend inline std::ostream& operator<<( std::ostream& os, const MTRand& mtrand );
+	friend inline std::istream& operator>>( std::istream& is, MTRand& mtrand );
 
 protected:
-	void initialize( const uint32 oneSeed );
-	void reload();
-	uint32 hiBit( const uint32& u ) const { return u & 0x80000000UL; }
-	uint32 loBit( const uint32& u ) const { return u & 0x00000001UL; }
-	uint32 loBits( const uint32& u ) const { return u & 0x7fffffffUL; }
-	uint32 mixBits( const uint32& u, const uint32& v ) const
+	inline void initialize( const uint32 oneSeed );
+	inline void reload();
+	inline uint32 hiBit( const uint32& u ) const { return u & 0x80000000UL; }
+	inline uint32 loBit( const uint32& u ) const { return u & 0x00000001UL; }
+	inline uint32 loBits( const uint32& u ) const { return u & 0x7fffffffUL; }
+	inline uint32 mixBits( const uint32& u, const uint32& v ) const
 		{ return hiBit(u) | loBits(v); }
-	uint32 twist( const uint32& m, const uint32& s0, const uint32& s1 ) const
+	inline uint32 twist( const uint32& m, const uint32& s0, const uint32& s1 ) const
 		{ return m ^ (mixBits(s0,s1)>>1) ^ (-loBit(s1) & 0x9908b0dfUL); }
-	static uint32 hash( time_t t, clock_t c );
+	inline static uint32 hash( time_t t, clock_t c );
 };
 
 
