2e6f81589f2126489ccec2ee5e2e5e9930328172
Spencer E. Olson <olsonse@umich.edu>
2009-10-03 14:24:55 -0600

    removed old files from cvs that didn't get updated correctly

--- examples/trapfe/testtrapfe.C
+++
@@ -1,9 +0,0 @@
-
-
-
-int main() {
-    double a = 0.0;
-    double b = 9.0;
-    double c = b / a;
-    return 0;
-}
--- src/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-int main() {
-    size_t hi = 0;
-    intptr_t there = 0;
-    //return (int)hi;
-    return there;
-return 0;
-}
